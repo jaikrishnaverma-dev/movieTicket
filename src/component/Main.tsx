fa6c04ab5ff468126065c59b3b52228d642f4d64
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2023-01-20 15:56:36 +0530

    compltewith nav

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -4,12 +4,35 @@ import Movielist from './Movielist'
 const Main = () => {
     return (
         <>
-          
-                <div className="container d-flex p-5" >
-                    <div className="col-6"><Movieform /></div>
-                    <div className="col-6"><Movielist /></div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">MoviesToWatch</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                        </ul>
+                        <span className="navbar-text">
+                            BoxOfice
+                        </span>
+                    </div>
                 </div>
-        
+            </nav>
+            <div className="container d-flex p-5" >
+                <div className="col-6"><Movieform /></div>
+                <div className="col-6"><Movielist /></div>
+            </div>
+
 
         </>
 
